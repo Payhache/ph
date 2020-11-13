5fa15520bc3ce8a3d4f2a25ea2bebfe3e60859b7
Pierre Henri <payhache@gmail.com>
2020-11-13 15:44:38 +0100

    factorisation

--- Portefolio/pfc/app.js
+++ Portefolio/pfc/app.js
@@ -1,106 +1,34 @@
 const launchGame = function () {
   let result;
+  let playerChoice;
+  let computerChoice;
+  let gameLimit = 10;
   let playerScore = 0;
   let computerScore = 0;
-  // Definit le nombre de parties à gagner
-  let gameLimit = 20;
-  // Selection de l'image a changer pour afficher choix comp
-  const computerChoiceImage = document.getElementById("imgComputerChoice");
-  // Selection des choix possibles par #id
+
   const choices = document.querySelectorAll(".choice");
-  // Selection des scores :
+  const btnReset = document.querySelector(".btn-reset");
+  const resultSentence = document.querySelector(".result");
   const playerScoreSPan = document.getElementById("user-score");
   const computerScoreSpan = document.getElementById("computer-score");
-  // Selection du texte pour annoncer le résultat :
-  let resultSentence = document.querySelector(".result");
-  // reset jeu avec le bouton
-  // selection du bouton reset :
-  const btnReset = document.querySelector(".btn-reset");
+  const computerChoiceImage = document.getElementById("imgComputerChoice");
+
   btnReset.addEventListener("click", resetGame);
-  /*Choix effectué par le joueur, une boucle est faite sur le
-tableau afin de récuperer le click  sur chaque élément */
+
   for (let i = 0; i < choices.length; i++) {
+    
     choices[i].addEventListener("click", function () {
-      let playerChoice;
-      let computerChoice;
-      // Affectation du choix du joueur
       playerChoice = choices[i].id;
-      // Affectation du choix de l'ordinateur
       computerChoice = defineComputerChoice();
-      // Gestion de l'affichage de l'image computer :
-      // Choix computer = rock
-      if(computerChoice === "r"){
-        computerChoiceImage.setAttribute("src", "img/pierre.png");
-      }
-      // Choix computer = paper
-      if(computerChoice === "p"){
-        computerChoiceImage.setAttribute("src", "img/feuille.png");
-      }
-      // Choix computer = cissors
-      if(computerChoice === "c"){
-        computerChoiceImage.setAttribute("src", "img/ciseau.png");
-      }
 
-      // Lancement de la fonction game pour Affichage du résultat
-      game(playerChoice, computerChoice);
-      
-      // Pas de vainqueur
-      if (result === "tie") {
-        resultSentence.textContent = "Egalité recommence !";
-        // Ajoute couleur grise autour du choix en cas de bonne réponse
-        applyResultStyle(playerChoice, "tie");
-        // Efface la couleur grise aprés quelques secondes
-        setTimeout(function () {
-          removeResultStyle(playerChoice, "tie");
-        }, 1000);
-        // Fin pas de vainqueur
-        // Joueur Gagne
-      } else if (result === "win") {
-        // Incrématation du score joueur en cas de victoire
-        playerScore++;
-        // Affichage du score du joueur
-        playerScoreSPan.textContent = playerScore;
-        resultSentence.textContent = ` Tu marques un point !  ${convertToWord(
-          playerChoice
-        )} bat ${convertToWord(computerChoice)}! `;
-        // Ajoute couleur verte autour du choix en cas de bonne réponse
-        applyResultStyle(playerChoice, "win");
-        // Efface la couleur verte aprés quelques secondes
-        setTimeout(function () {
-          removeResultStyle(playerChoice, "win");
-        }, 1000);
-        // Fin joueur Gagne
-        // Joueur perd
-      } else if (result === "loose") {
-        computerScore++;
-        computerScoreSpan.textContent = computerScore;
-        resultSentence.textContent = `Tu as perdu ! ${convertToWord(
-          computerChoice
-        )} bat ${convertToWord(playerChoice)} !`;
-        // Ajoute couleur rouge autour du choix en cas de bonne réponse
-        applyResultStyle(playerChoice, "loose");
-        // Efface la couleur rouge aprés quelques secondes
-        setTimeout(function () {
-          removeResultStyle(playerChoice, "loose");
-        }, 1000);
-        // Fin joueur perd
-      }
-      // reset du jeu avec le score:
-      if (computerScore == gameLimit || playerScore == gameLimit) {
-        if (computerScore > playerScore) {
-          resultSentence.textContent = "Tu as perdu  la partie";
-        } else {
-          resultSentence.textContent = "Tu as gagné la partie";
-        }
-        // attend 2 seconde avant de reset le game pour permettre l'affichage du score
-        setTimeout(resetGame, 2000);
-      }
-     
-
+      displayImageComputerChoice(computerChoice);
+      gameStart(playerChoice, computerChoice);
+      displayResult(result);
+      checkIsgameOver();
     });
   }
-  // Comparaison des résultats :
-  function game(playerChoice, computerChoice) {
+
+  function gameStart(playerChoice, computerChoice) {
     switch (playerChoice + computerChoice) {
       case "rr":
       case "pp":
@@ -120,6 +48,58 @@ tableau afin de récuperer le click  sur chaque élément */
     }
   }
 
+  function displayResult(result) {
+    if (result === "tie") {
+
+      displayTextResult("Egalité recommence !");
+      applyResultStyle(playerChoice, "tie");
+      setTimeout(function () {removeResultStyle(playerChoice, "tie");
+      }, 1000);
+
+    } else if (result === "win") {
+
+      displayScoreResult(playerScoreSPan, playerScore++);
+      displayTextResult(`Tu marques un point !  ${convertToWord(playerChoice)} bat ${convertToWord(computerChoice)}!`);
+      applyResultStyle(playerChoice, "win");
+      setTimeout(function () {
+        removeResultStyle(playerChoice, "win");
+      }, 1000);
+
+    } else if (result === "loose") {
+
+      displayScoreResult(computerScoreSpan, computerScore++)
+      displayTextResult(`Tu as perdu ! ${convertToWord(computerChoice)} bat ${convertToWord(playerChoice)} !`);
+      applyResultStyle(playerChoice, "loose");
+      setTimeout(function () {
+        removeResultStyle(playerChoice, "loose");
+      }, 1000);
+    } 
+  }
+
+  function checkIsgameOver(){
+    if (computerScore == gameLimit || playerScore == gameLimit) {
+      if (computerScore > playerScore) {
+        displayTextResult("Tu as perdu  la partie");
+      } else {
+        displayTextResult("Tu as gagné la partie");
+      }
+      setTimeout(resetGame, 2000);
+    }    
+  }
+
+
+  function displayImageComputerChoice(computerChoice) {
+    if(computerChoice === "r"){
+      choiseImageTodisplay("pierre")
+    }
+    if(computerChoice === "p"){
+      choiseImageTodisplay("feuille")
+    }
+    if(computerChoice === "c"){
+      choiseImageTodisplay("ciseau")
+    }
+  }
+
   function applyResultStyle(choice, cssClass) {
     document.getElementById(choice).classList.add(cssClass);
   }
@@ -133,18 +113,29 @@ tableau afin de récuperer le click  sur chaque élément */
     return  choices[randomNumber].id
   }
 
-  // Fonction pour afficher le resultat en toute lettre
   function convertToWord(letter) {
     if (letter === "r") return " la Pierre";
     if (letter === "p") return " le Papier";
     if (letter === "c") return "les Ciseaux";
   }
-  // Fonction pour reset le jeu
+
   function resetGame() {
     playerScore = 0;
     computerScore = 0;
     computerScoreSpan.textContent = computerScore;
     playerScoreSPan.textContent = playerScore;
+  }
+
+  function choiseImageTodisplay(img) {
+    computerChoiceImage.setAttribute("src", `img/${img}.png`);
+  }
+
+  function displayTextResult(textResult){
+    resultSentence.textContent = textResult;
+  }
+
+  function displayScoreResult(content, score) {
+    content.textContent = score;
   }
 };
 
